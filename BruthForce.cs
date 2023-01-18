99c5b18b0df23bd1d600d19fd87ae8ab31b3974b
Jaywapp <jaywapp16@gmail.com>
2023-01-19 00:09:25 +0900

    code cleaning

--- BruthForce.cs
+++ BruthForce.cs
@@ -5,7 +5,7 @@
         public static int Match(string text, string pattern)
         {
             // text char 반복
-            for (int i = 0; i< text.Length - pattern.Length; i++)
+            for (int i = 0; i < text.Length - pattern.Length; i++)
             {
                 int j = 0;
                 // pattern char를 반복하면서 해당 위치의 text char와 비교
@@ -14,7 +14,7 @@
 
                 // pattern와 text의 부분 문자열이 같을 경우
                 var isMatched = j == pattern.Length;
-                if(isMatched)
+                if (isMatched)
                     return i;
             }
 
