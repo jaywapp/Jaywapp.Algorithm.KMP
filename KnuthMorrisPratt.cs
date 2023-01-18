137757ed4fdf582e12c62f3e726ab9baaa7110e1
junyoungpark10 <junyoung.park@penta-cube.com>
2023-01-18 17:16:14 +0900

    initialize

---
+++ KnuthMorrisPratt.cs
@@ -0,0 +1,66 @@
+﻿namespace Jaywapp.KMP
+{
+    public static class KnuthMorrisPratt
+    {
+        public static int Match(string text, string pattern)
+        {
+			int n = text.Length;
+			int m = pattern.Length;
+
+			var array = PrefixFunction(pattern);
+
+			int i = 0;
+			int j = 0;
+			while (i < n)
+			{
+				if (text[i] == pattern[j])
+					if (j == m - 1)
+						return i - j;
+					else
+					{
+						i++;
+						j++;
+					}
+				else
+				{
+					if (j > 0)
+						j = array[j - 1];
+					else
+						i++;
+				}
+			}
+
+			return -1;
+		}
+
+		public static int[] PrefixFunction(string pattern)
+		{
+            var array = new int[pattern.Length];
+
+            array[0] = 0;
+			int i = 1;
+			int j = 0;
+
+			while (i < pattern.Length)
+			{
+				if (pattern[i] == pattern[j])
+				{
+					array[i] = j + 1;
+					i++;
+					j++;
+				}
+				else if (j > 0)
+				{
+					j = array[j - 1];
+				}
+				else
+				{
+					array[j] = 0;
+					i++;
+				}
+			}
+
+			return array;
+		}
+	}
+}
---
+++ Program.cs
@@ -0,0 +1,94 @@
+﻿using System;
+
+namespace Jaywapp.KMP
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            RunPrefixFunction();
+            // RunStringMatch();
+        }
+
+        private static void RunPrefixFunction()
+        {
+            while (true)
+            {
+                Console.Write("pattern : ");
+                var pattern = Console.ReadLine();
+                if (pattern == "EOF")
+                    break;
+
+                var array = KnuthMorrisPratt.PrefixFunction(pattern);
+                var chars = pattern.ToCharArray();
+
+                Console.WriteLine(string.Join(" | ", chars));
+                Console.WriteLine(string.Join(" | ", array));
+            }
+
+            Console.ReadLine();
+        }
+
+
+        private static void RunStringMatch()
+        {
+            while (true)
+            {
+                Console.Write("text : ");
+                var text = Console.ReadLine();
+                if (text == "EOF")
+                    break;
+                Console.Write("pattern : ");
+                var pattern = Console.ReadLine();
+                if (pattern == "EOF")
+                    break;
+
+                if (!TryRun(BruthForce.Match, text, pattern, out int bruthForthIndex, out TimeSpan bruthForthPerformance)
+                    || !TryRun(KnuthMorrisPratt.Match, text, pattern, out int kmpIndex, out TimeSpan kmpPerformance))
+                    break;
+
+                Report(bruthForthIndex, bruthForthPerformance, kmpIndex, kmpPerformance);
+            }
+
+            Console.ReadLine();
+        }
+
+        private static void Report(
+            int bruthForthIndex, TimeSpan bruthForthPerformance,
+            int kmpIndex, TimeSpan kmpPerformance)
+        {
+            Console.WriteLine("---------------------------------");
+
+            Console.WriteLine("* Bruth Forth");
+            Console.WriteLine($"Index : {bruthForthIndex} / Performance : {bruthForthPerformance}");
+
+            Console.WriteLine("* KMP");
+            Console.WriteLine($"Index : {kmpIndex} / Performance : {kmpPerformance}");
+
+            Console.WriteLine("---------------------------------");
+        }
+
+        private static bool TryRun(Func<string, string, int> func, string text, string pattern, out int index, out TimeSpan performance)
+        {
+            index = -1;
+            performance = default;
+
+            try
+            {
+                var start = DateTime.Now;
+                var result = func(text, pattern);
+                var end = DateTime.Now;
+
+                index = result;
+                performance = end - start;
+
+                return true;
+            }
+            catch
+            {
+
+                return false;
+            }
+        }
+    }
+}
